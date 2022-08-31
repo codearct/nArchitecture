2365dbf0b7a0c6be597a348af35d9e62169d22d4
codearct <codearct@gmail.com>
2022-08-31 23:37:35 +0300

    CQRS Devam

--- src/demoProjects/rentACar/Application/Features/Brands/Rules/BrandBusinessRules.cs
+++ src/demoProjects/rentACar/Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -22,6 +22,14 @@ public class BrandBusinessRules
     public async Task BrandNameCanNotBeDuplicatedWhenInserted(string name)
     {
         IPaginate<Brand> result = await _brandRepository.GetListAsync(b => b.Name == name);
-        if (result.Items.Any()) throw new BusinessException("Brand name exists.");
+
+        if (result.Items.Any()) 
+            throw new BusinessException("Brand name exists.");
+    }
+
+    public void BrandShouldExistWhenRequested(Brand brand)
+    {
+        if (brand == null) 
+            throw new BusinessException("Requested brand does not exist");
     }
 }
